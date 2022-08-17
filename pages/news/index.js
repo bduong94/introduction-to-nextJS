4cdbc1a323d03a7173e3c1767a7712f84b8a0dbf
Brian Duong <brianminhduong@gmail.com>
2022-08-16 20:51:47 -0400

    Utilize Link component to Link pages on app

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 function NewsPage() {
   return (
     <>
       <h1>The News Page</h1>
       <ul>
-        <li>NextJS Is a Great Framework</li>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is a Great Framework
+          </Link>
+        </li>
         <li>Something else</li>
       </ul>
     </>
