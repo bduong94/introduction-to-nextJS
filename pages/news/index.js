b0ec9b77aec7559c7a4eafc21cefa85df533c3bd
Brian Duong <brianminhduong@gmail.com>
2022-08-16 20:43:57 -0400

    Add content to NewsPage

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,13 @@
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>NextJS Is a Great Framework</li>
+        <li>Something else</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
